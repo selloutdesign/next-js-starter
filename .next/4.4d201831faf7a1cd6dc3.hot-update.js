64033816b9ccdfcab673b2835e4b8af65ef80830
Josh <j.selloutdesign@gmail.com>
2018-05-03 09:43:02 -0700

    first commit

---
+++ .next/4.4d201831faf7a1cd6dc3.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: /Users/joshsells/Sites/hello-next/pages/index.js: Unterminated JSX contents (4:8)\n\n  2 | \t\t<div>\n  3 | \t\t\t<p>Hello Next.js\n> 4 | \t\t</div>\n    | \t\t      ^\n  5 | \t)\n  6 | \n  7 | export default Index\n    at _class.raise (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:778:15)\n    at _class.jsxReadToken (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:7727:16)\n    at _class.readToken (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:8095:21)\n    at _class.nextToken (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:976:12)\n    at _class.next (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:912:10)\n    at _class.eat (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:917:12)\n    at _class.expect (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:2051:10)\n    at _class.jsxParseClosingElementAt (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:8001:12)\n    at _class.jsxParseElementAt (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:8020:39)\n    at _class.jsxParseElementAt (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:8024:34)\n    at _class.jsxParseElement (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:8077:19)\n    at _class.parseExprAtom (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:8084:21)\n    at _class.parseExprSubscripts (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:2725:21)\n    at _class.parseMaybeUnary (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:2704:21)\n    at _class.parseExprOps (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:2616:21)\n    at _class.parseMaybeConditional (/Users/joshsells/Sites/hello-next/node_modules/babylon/lib/index.js:2588:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=4.4d201831faf7a1cd6dc3.hot-update.js.map
---
+++ .next/4.e4d1e4e12d1dd4aca6c9.hot-update.js
@@ -0,0 +1,143 @@
+webpackHotUpdate(4,{
+
+/***/ "./components/MyLayout.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/react/cjs/react.development.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Header__ = __webpack_require__("./components/Header.js");
+var _jsxFileName = "/Users/joshsells/Sites/hello-next/components/MyLayout.js";
+
+
+(function () {
+  var enterModule = __webpack_require__("./node_modules/react-hot-loader/index.js").enterModule;
+
+  enterModule && enterModule(module);
+})();
+
+
+var layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
+var Layout = function Layout(props) {
+  __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    style: layoutStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1__Header__["a" /* default */], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    }
+  }), props.Children);
+};
+
+var _default = Layout;
+/* harmony default export */ __webpack_exports__["a"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = __webpack_require__("./node_modules/react-hot-loader/index.js").default;
+
+  var leaveModule = __webpack_require__("./node_modules/react-hot-loader/index.js").leaveModule;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(layoutStyle, "layoutStyle", "/Users/joshsells/Sites/hello-next/components/MyLayout.js");
+  reactHotLoader.register(Layout, "Layout", "/Users/joshsells/Sites/hello-next/components/MyLayout.js");
+  reactHotLoader.register(_default, "default", "/Users/joshsells/Sites/hello-next/components/MyLayout.js");
+  leaveModule(module);
+})();
+
+;
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/react/cjs/react.development.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_MyLayout_js__ = __webpack_require__("./components/MyLayout.js");
+var _jsxFileName = "/Users/joshsells/Sites/hello-next/pages/index.js";
+
+
+(function () {
+  var enterModule = __webpack_require__("./node_modules/react-hot-loader/index.js").enterModule;
+
+  enterModule && enterModule(module);
+})();
+
+
+
+var _default = function _default() {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1__components_MyLayout_js__["a" /* default */], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(Header, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, "Hello Next.js"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = __webpack_require__("./node_modules/react-hot-loader/index.js").default;
+
+  var leaveModule = __webpack_require__("./node_modules/react-hot-loader/index.js").leaveModule;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(_default, "default", "/Users/joshsells/Sites/hello-next/pages/index.js");
+  leaveModule(module);
+})();
+
+;
+    (function (Component, route) {
+      if(!Component) return
+      if (false) return
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=4.e4d1e4e12d1dd4aca6c9.hot-update.js.map
